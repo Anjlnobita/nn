6aa41d12ead28004130390034b92693accae9554
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-30 05:37:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,20 +1,22 @@
-# ©️biisal jai shree krishna 😎
-from os import environ
-from dotenv import load_dotenv
 
-load_dotenv()
+from os import environ
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
-MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
-AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-1001734958816")
-)
-FSUB = environ.get("FSUB", True)
+API_ID = 20650066
+API_HASH =  "7a4f8ed638f1369a40693574c2835217"
+
+BOT_TOKEN = "7963650428:AAGvbf9QIAWsZRhv9QGxka33OqqGQtX4axc"
+
+ADMIN = 6777860063
+
+CHAT_GROUP = -1002044136776
+
+LOG_CHANNEL = -1002044136776
+
+MONGO_URL = "mongodb+srv://anjlnobita:tCUPU9Ty1FFvLumv@cluster0.appf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+AUTH_CHANNEL = -1002044136776
+
+FSUB =  True
 STICKERS_IDS = (
     "CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME"
 ).split()
@@ -22,4 +24,7 @@ COOL_TIMER = 20  # keep this atleast 20
 ONLY_SCAN_IN_GRP = environ.get(
     "ONLY_SCAN_IN_GRP", True
 )  # If IMG_SCAN_IN_GRP is set to True, image scanning is restricted to your support group only. If it's False, the image scanning feature can be used anywhere.
+
 REACTIONS = ["❤️‍🔥", "⚡", "🔥"]
+
+
